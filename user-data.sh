529524d16dd5f782de2f6440add4c604477bd5d6
swethapothireddy <122109250+swethapothireddy@users.noreply.github.com>
2025-12-18 19:39:22 +0530

    Update user-data.sh
    Updated user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
+# User-data script for Auto Scaling EC2 instances
+
+# Update system
 yum update -y
+
+# Install Docker
 amazon-linux-extras install docker -y
+
+# Start Docker service
 systemctl start docker
 systemctl enable docker
-docker run -d -p 80:80 nginx
+
+# Allow ec2-user to run docker commands
+usermod -a -G docker ec2-user
+
+# Pull Docker image from Docker Hub
+docker pull swethareddy2/autohealing-web:latest
+
+# Run NGINX container
+docker run -d \
+  --name autohealing-web \
+  -p 80:80 \
+  --restart always \
+  swethareddy2/autohealing-web:latest
